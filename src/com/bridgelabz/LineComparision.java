260e5e9a761a8d4fe7be5aef3b36a7e1eba661f0
“shubham767676” <shubhamv.singh06@gmail.com>
2022-02-20 07:25:26 +0530

    Added: Check Eqaulity

--- src/com/bridgelabz/LineComparision.java
+++ src/com/bridgelabz/LineComparision.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class LineComparision {
 	public static double coOrdinates() {
 		Scanner sc = new Scanner(System.in);
-
 		double x1, x2, y1, y2;
 
 		System.out.println("Enter Co-ordinates of First Point");
@@ -16,14 +15,28 @@ public class LineComparision {
 		x2 = sc.nextDouble();
 		y2 = sc.nextDouble();
 
-		double lengthOfLine = (double) Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-		return lengthOfLine;
+		double lenghOfLine = (double) Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+		return lenghOfLine;
+	}
+
+	public static void checkEquality(double lineOneLength, double lineTwoLength) {
+		if (lineOneLength == lineTwoLength) {
+			System.out.println("Lines are Equal");
+		} else {
+			System.out.println("Lines are not Equal");
+		}
+
 	}
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Line Comparison Computation ");
-		double lengthOfLine = coOrdinates();
-		System.out.println("Length Of Line is: " + lengthOfLine);
+		System.out.println("Welcome to Line Comparision Computation");
+		double lineOneLength = coOrdinates();
+		System.out.println("Length Of Line1 is: " + lineOneLength);
+		double lineTwoLength = coOrdinates();
+		System.out.println("Length Of Line2 is: " + lineTwoLength);
+
+		checkEquality(lineOneLength, lineTwoLength);
 
 	}
+
 }
